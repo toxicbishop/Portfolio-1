65ff4d88a92e46bfc171cae6498f8e86e0ae0461
toxicbishop <pranavarun19@gmail.com>
2026-07-25 10:07:57 +0530

    fix: resolve minimatch vulnerability and use next/link in Navbar to fix Vercel build

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Socials } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="w-screen md:w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10 m-0 max-w-[1855px] items-center rounded-full">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[0px] md:px-[10px]">
-        <a href="/" className="h-auto w-auto flex flex-row items-center">
+        <Link href="/" className="h-auto w-auto flex flex-row items-center">
           <div className="h-[40px] w-[40px] rounded-full overflow-hidden border border-[#7042f88b]">
             <Image
               src="/assets/pranav.png"
@@ -20,7 +21,7 @@ const Navbar = () => {
           <span className="font-bold ml-[10px] block text-gray-300 z-50 md:text-lg text-xl">
             Pranav Arun
           </span>
-        </a>
+        </Link>
 
         <div className="hidden w-3/6 lg:w-1/3 h-full md:flex flex-row items-center justify-between md:mx-auto lg:pr-12">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
